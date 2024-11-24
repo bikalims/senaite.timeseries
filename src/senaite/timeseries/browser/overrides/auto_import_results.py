8e6cc10eb9a79458f4a33795a3d68bddeb06d195
Mike Metcalfe <mike@webtide.co.za>
2024-11-24 15:09:29 +0200

    Added auto import override

---
+++ src/senaite/timeseries/browser/overrides/auto_import_results.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+# from bika.lims import api
+from senaite.core.exportimport.auto_import_result import AutoImportResultsView as AIRV
+
+
+class AutoImportResultsView(AIRV):
+    pass
